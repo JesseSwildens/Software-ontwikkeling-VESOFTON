684653fde3cf38a518db740e4f222c38fb23475e
JesseSwildens <swildensjesse@gmail.com>
2024-05-05 17:57:08 +0200

    #13 Create an empty file for the artefact step. And changed the checking file back to xml

--- app/cppcheck.sh
+++ app/cppcheck.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
+# Creating an empty file so nothing fails that expects a result file to be present
+touch $(dirname "$0")/cppcheck-result.xml
+
 # $n == argument n (0 == file, 1 == argument 1, etc..)
 # (>) redirects, 1 == stdout, 2 == stderr
 cppcheck $1 --cppcheck-build-dir=$(dirname "$0")/build-cppcheck --force --xml 2> $(dirname "$0")/cppcheck-result.xml
 
-if [ -s $(dirname "$0")/cppcheck-result.log ]; then
+if [ -s $(dirname "$0")/cppcheck-result.xml ]; then
     # Something is inside the error result file
-    echo $(<$(dirname "$0")/cppcheck-result.log)
+    echo $(<$(dirname "$0")/cppcheck-result.xml)
     exit 1 # Try again ;)
 else
     exit 0
