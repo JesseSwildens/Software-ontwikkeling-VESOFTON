45e4affc77ebe9ce6d62c62904710a4d26f6aab1
JesseSwildens <swildensjesse@gmail.com>
2024-05-05 17:50:50 +0200

    #13 Force fail the build if the check errors isn't empty. So a new error has been introduced. And changed the format back to xml as no logging is passed through to file.

--- app/cppcheck.sh
+++ app/cppcheck.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-cppcheck $1 --cppcheck-build-dir=$(dirname "$0")/build-cppcheck --check-level=exhaustive --force --xml 2>&1 | tee $(dirname "$0")/cppcheck-result.log
+# $n == argument n (0 == file, 1 == argument 1, etc..)
+# (>) redirects, 1 == stdout, 2 == stderr
+cppcheck $1 --cppcheck-build-dir=$(dirname "$0")/build-cppcheck --force --xml 2> $(dirname "$0")/cppcheck-result.xml
+
+if [ -s $(dirname "$0")/cppcheck-result.log ]; then
+    # Something is inside the error result file
+    echo $(<$(dirname "$0")/cppcheck-result.log)
+    exit 1 # Try again ;)
+else
+    exit 0
+fi
