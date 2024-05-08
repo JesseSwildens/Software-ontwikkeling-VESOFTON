38f5071519b8468cf466c822e6242097f5f4abdf
Tom Selier <tbj.selier@gmail.com>
2024-05-08 17:53:04 +0200

    #21 moved add_str to helpers.py

---
+++ terminal/helpers.py
@@ -0,0 +1,13 @@
+import dearpygui.dearpygui as dpg
+
+def add_str(tag, string):
+    if string is None or string == "":
+        return
+
+    old_string = dpg.get_value("Display")
+    count = old_string.count("\n")
+    if count > 21:
+        token = old_string.split("\n")
+        token.pop(0)
+        old_string = "\n".join(token)
+    dpg.set_value(tag,  old_string + "\n" + string)
