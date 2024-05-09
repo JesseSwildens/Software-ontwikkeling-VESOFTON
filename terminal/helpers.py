8ca1080fec842f654e6deced6fd78d7b4c1ea7d5
Tom Selier <tbj.selier@gmail.com>
2024-05-09 19:20:28 +0200

    #21 fixed bug where window resize would break displayed lines

--- terminal/helpers.py
+++ terminal/helpers.py
@@ -6,8 +6,9 @@ def add_str(tag, string):
 
     old_string = dpg.get_value("Display")
     count = old_string.count("\n")
-    if count > ((dpg.get_viewport_height() - 130) // 13):
+    while count > ((dpg.get_viewport_height() - 130) // 13):
         token = old_string.split("\n")
         token.pop(0)
         old_string = "\n".join(token)
+        count = old_string.count("\n")
     dpg.set_value(tag,  old_string + "\n" + string)
