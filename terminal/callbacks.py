e10456bf79c4a63806a18f0ffd79b5170659d6a2
Tom Selier <tbj.selier@gmail.com>
2024-05-14 21:05:58 +0200

    #36 added buttons with callbacks to imported text window

--- terminal/callbacks.py
+++ terminal/callbacks.py
@@ -3,6 +3,17 @@ import dearpygui.dearpygui as dpg
 from display import add_display, clear_display
 from serial_handler import SerialHandler
 
+def find_current_string():
+    tag = ""
+    for idx in range(100):
+        color = dpg.get_item_configuration(f"_import_{idx}")['color']
+        color = [int(x*255) for x in color] # wonky color :)
+        print(color)
+        if color == [255, 255, 0, 255]:
+            break
+
+    return tag
+
 def command_callback(_sender, _app_data, _user_data : SerialHandler):
     command = dpg.get_value("_command")
     _user_data.send(command)
@@ -11,12 +22,21 @@ def command_callback(_sender, _app_data, _user_data : SerialHandler):
     dpg.focus_item("_command")
 
 def button_callback(_sender, _app_data, _user_data : SerialHandler):
-    command = dpg.get_value("_command")
-    _user_data.send(command)
-    add_display(command, True)
-    dpg.set_value("_command", "")
+    print(_sender)
+    if _sender == "_send":
+        command = dpg.get_value("_command")
+        _user_data.send(command)
+        add_display(command, True)
+        dpg.set_value("_command", "")
+    elif _sender == "_step_over":
+        print("_step_over called")
+        find_current_string()
+    elif _sender == "_skip":
+        pass
+    elif _sender == "_send_all":
+        pass
 
-def resize_callback(_sender, _app_data, _user_data : SerialHandler):
+def viewport_resize_callback(_sender):
     dpg.set_item_pos("_send", (10, dpg.get_viewport_height()-75))
     dpg.set_item_pos(
         "_command", 
@@ -25,6 +45,14 @@ def resize_callback(_sender, _app_data, _user_data : SerialHandler):
         "_status", 
         (dpg.get_item_width("_send") + dpg.get_item_width("_command") + 50,
          dpg.get_viewport_height()-75))
+
+def window_resize_callback(_sender):
+    dpg.set_item_pos("_step_over", (dpg.get_item_width("_imported_text")-100,
+                                    dpg.get_item_height("_imported_text")-90))
+    dpg.set_item_pos("_skip", (dpg.get_item_width("_imported_text")-100,
+                               dpg.get_item_height("_imported_text")-60))
+    dpg.set_item_pos("_send_all", (dpg.get_item_width("_imported_text")-100,
+                                   dpg.get_item_height("_imported_text")-30))
 
 def menu_callback(sender, _app_data, user_data : SerialHandler):
     enable_items = ["_command", "_send"]
@@ -43,15 +71,8 @@ def menu_callback(sender, _app_data, user_data : SerialHandler):
         dpg.configure_item(item, enabled=enable)
 
 def ok_callback(_sender, app_data):
-    print('OK was clicked.')
-
-    with open(app_data['file_path_name']) as file:
+    with open(app_data['file_path_name'], 'r') as file:
         for idx, line in enumerate(file):
-            print(line.strip("\r").strip('\n'))
             dpg.set_value(f"_import_{idx}", line.strip("\r").strip('\n'))
         dpg.configure_item("_imported_text", show=True)
-
-def cancel_callback(_sender, _app_data):
-    print('Cancel was clicked.')
-    # print("Sender: ", sender)
-    # print("App Data: ", app_data)
+        dpg.configure_item("_import_0", color=(255, 255, 0, 255))
--- terminal/main.py
+++ terminal/main.py
@@ -14,7 +14,7 @@ ser = SerialHandler()
 # set up a viewport
 dpg.create_context()
 dpg.create_viewport(title="Terminal", width=1280, height=960, resizable=True)
-dpg.set_viewport_resize_callback(callbacks.resize_callback)
+dpg.set_viewport_resize_callback(callbacks.viewport_resize_callback)
 
 # Main window
 with dpg.window(tag="_primary"):
@@ -45,21 +45,56 @@ with dpg.window(tag="_primary"):
         (dpg.get_viewport_width()-len(dpg.get_value("_status"))-100, 20))
 
 # File dialog
-with dpg.file_dialog(directory_selector=False, show=False, 
+with dpg.file_dialog(directory_selector=False, show=False,
                     callback=callbacks.ok_callback, tag="_file_dialog",
-                    cancel_callback=callbacks.cancel_callback, 
                     width=700 ,height=400):
-    dpg.add_file_extension(".txt")
+    dpg.add_file_extension(".txt", color=(255, 255, 0, 255))
 
 # Window for imported text.
-with dpg.window(tag="_imported_text", show=False):
+with dpg.window(tag="_imported_text", show=False, autosize=False,
+                width=700, height=400, no_scrollbar=True):
+    # Add lines for imported text
     for idx in range(100):
-        dpg.add_text("", tag=f"_import_{idx}", pos=(10, 10+idx*13))
+        dpg.add_text("", tag=f"_import_{idx}", pos=(20, 20+idx*13))
+
+    # Add buttons
+    dpg.add_button(
+        label="Step over",
+        tag="_step_over",
+        pos=(
+            dpg.get_item_width("_imported_text")-100,
+            dpg.get_item_height("_imported_text")-90),
+        width=80,
+        callback=callbacks.button_callback,
+        user_data=ser)
+    dpg.add_button(
+        label="Skip",
+        tag="_skip",
+        pos=(
+            dpg.get_item_width("_imported_text")-100,
+            dpg.get_item_height("_imported_text")-60),
+        width=80,
+        callback=callbacks.button_callback,
+        user_data=ser)
+    dpg.add_button(
+        label="Send all",
+        tag="_send_all",
+        pos=(
+            dpg.get_item_width("_imported_text")-100,
+            dpg.get_item_height("_imported_text")-30),
+        width=80,
+        callback=callbacks.button_callback,
+        user_data=ser)
+
+# Handler for window resizing (instead of viewport)
+with dpg.item_handler_registry(tag="_window_handler"):
+    dpg.add_item_resize_handler(callback=callbacks.window_resize_callback)
+dpg.bind_item_handler_registry("_imported_text", "_window_handler")
 
 # Menu bar
 with dpg.viewport_menu_bar():
     with dpg.menu(label="File"):
-        dpg.add_menu_item(label="Open", 
+        dpg.add_menu_item(label="Open",
                           callback=lambda:dpg.show_item("_file_dialog"))
     with dpg.menu(label="Port"):
         for port in ser.available_ports:
@@ -83,12 +118,12 @@ with dpg.viewport_menu_bar():
         dpg.add_menu_item(
             label="Show Item Registry",
             callback=lambda:dpg.show_tool(dpg.mvTool_ItemRegistry))
-        
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.set_primary_window("_primary", True)
 
+# Render loop
 while dpg.is_dearpygui_running():
     data = ser.poll()
     if data is not None:
