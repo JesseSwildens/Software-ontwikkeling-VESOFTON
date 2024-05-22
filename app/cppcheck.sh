ccacf31970178f8a873faf1b4e605e0bc4fe28bd
Jesse <swildensjesse@gmail.com>
2024-05-22 17:02:08 +0200

    #41 Revert shell script test and added action in actions script

--- app/cppcheck.sh
+++ app/cppcheck.sh
@@ -3,7 +3,6 @@
 # Creating an empty file so nothing fails that expects a result file to be present
 touch $(dirname "$0")/cppcheck-result.xml
 
-cppcheck --version | echo
 # $n == argument n (0 == file, 1 == argument 1, etc.., $@ == all arguments)
 # (>) redirects, 1 == stdout, 2 == stderr
 cppcheck --cppcheck-build-dir=$(dirname "$0")/build-cppcheck --check-level=exhaustive --xml $@ 2> $(dirname "$0")/cppcheck-result.xml
