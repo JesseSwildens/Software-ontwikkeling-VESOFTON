23b4b7622f9532c831fa4c021f43ebbcf1d43b8e
Tom Selier <tbj.selier@gmail.com>
2024-05-29 11:14:48 +0200

    #40 formatting

--- terminal/callbacks.py
+++ terminal/callbacks.py
@@ -1,41 +1,50 @@
-import time
 import os.path
+import time
 
 import dearpygui.dearpygui as dpg
-
+from bitmap import convert_to_bitmap
 from display import add_display, clear_display
 from serial_handler import SerialHandler
-from bitmap import convert_to_bitmap
+
 
 def find_current_string():
     for idx in range(100):
-        color = dpg.get_item_configuration(f"_import_{idx}")['color']
-        color = [int(x*255) for x in color] # wonky color :)
+        color = dpg.get_item_configuration(f"_import_{idx}")["color"]
+        color = [int(x * 255) for x in color]  # wonky color :)
         if color == [255, 255, 0, 255]:
             break
     return idx
 
+
 def highlight_next(tag_without_idx, idx):
     next = 1
     try:
-        while dpg.get_value(tag_without_idx + str(idx+next))\
-                           .strip("\n").strip("\r") == "":
+        while (
+            dpg.get_value(tag_without_idx + str(idx + next)).strip("\n").strip("\r")
+            == ""
+        ):
             next += 1
     except AttributeError:
         next = -idx
 
-    dpg.configure_item(tag_without_idx + str(idx+next), color = [
-            int(x*255) for x in
-            dpg.get_item_configuration(tag_without_idx + str(idx))['color']])
-    dpg.configure_item(tag_without_idx + str(idx), color = [255, 255, 255, 255])
-    return idx+next
+    dpg.configure_item(
+        tag_without_idx + str(idx + next),
+        color=[
+            int(x * 255)
+            for x in dpg.get_item_configuration(tag_without_idx + str(idx))["color"]
+        ],
+    )
+    dpg.configure_item(tag_without_idx + str(idx), color=[255, 255, 255, 255])
+    return idx + next
 
-def command_callback(_sender, _app_data, user_data : SerialHandler):
+
+def command_callback(_sender, _app_data, user_data: SerialHandler):
     command = dpg.get_value("_command")
     user_data.send(command)
     add_display(command, True)
     dpg.set_value("_command", "")
     dpg.focus_item("_command")
+
 
 def button_callback(sender, _app_data, user_data):
     if sender == "_send":
@@ -53,11 +62,11 @@ def button_callback(sender, _app_data, user_data):
         highlight_next("_import_", idx)
     elif sender == "_send_all":
         idx = find_current_string()
-        while(highlight_next("_import_", idx) != 0):
+        while highlight_next("_import_", idx) != 0:
             command = dpg.get_value(f"_import_{idx}")
             user_data.send(command)
             add_display(command, True)
-            time.sleep(dpg.get_value("_interval_slider")/1000)
+            time.sleep(dpg.get_value("_interval_slider") / 1000)
             idx = find_current_string()
     elif sender == "_convert":
         # user_data is a path in this case
@@ -65,50 +74,70 @@ def button_callback(sender, _app_data, user_data):
     else:
         print(sender)
 
+
 def viewport_resize_callback(_sender):
-    dpg.set_item_pos("_send", (10, dpg.get_viewport_height()-75))
+    dpg.set_item_pos("_send", (10, dpg.get_viewport_height() - 75))
     dpg.set_item_pos(
-        "_command",
-        (dpg.get_item_width("_send") + 25, dpg.get_viewport_height()-75))
+        "_command", (dpg.get_item_width("_send") + 25, dpg.get_viewport_height() - 75)
+    )
     dpg.set_item_pos(
         "_status",
-        (dpg.get_item_width("_send") + dpg.get_item_width("_command") + 50,
-         dpg.get_viewport_height()-75))
+        (
+            dpg.get_item_width("_send") + dpg.get_item_width("_command") + 50,
+            dpg.get_viewport_height() - 75,
+        ),
+    )
+
 
 def window_resize_callback(_sender):
-    dpg.set_item_pos("_step_over", (dpg.get_item_width("_imported_text")-100,
-                                    dpg.get_item_height("_imported_text")-90))
-    dpg.set_item_pos("_skip", (dpg.get_item_width("_imported_text")-100,
-                               dpg.get_item_height("_imported_text")-60))
-    dpg.set_item_pos("_send_all", (dpg.get_item_width("_imported_text")-100,
-                                   dpg.get_item_height("_imported_text")-30))
+    dpg.set_item_pos(
+        "_step_over",
+        (
+            dpg.get_item_width("_imported_text") - 100,
+            dpg.get_item_height("_imported_text") - 90,
+        ),
+    )
+    dpg.set_item_pos(
+        "_skip",
+        (
+            dpg.get_item_width("_imported_text") - 100,
+            dpg.get_item_height("_imported_text") - 60,
+        ),
+    )
+    dpg.set_item_pos(
+        "_send_all",
+        (
+            dpg.get_item_width("_imported_text") - 100,
+            dpg.get_item_height("_imported_text") - 30,
+        ),
+    )
 
-def menu_callback(sender, _app_data, user_data : SerialHandler):
+
+def menu_callback(sender, _app_data, user_data: SerialHandler):
     enable_items = ["_command", "_send"]
 
     if user_data.connect(sender):
         dpg.set_value("_status", "Connected")
-        dpg.configure_item("_status", color=(0, 255, 0, 255)) # Red
+        dpg.configure_item("_status", color=(0, 255, 0, 255))  # Red
         clear_display()
         enable = True
     else:
         dpg.set_value("_status", "Disconnected")
-        dpg.configure_item("_status", color=(255, 0, 0, 255)) # Green
+        dpg.configure_item("_status", color=(255, 0, 0, 255))  # Green
         enable = False
 
     for item in enable_items:
         dpg.configure_item(item, enabled=enable)
 
-def warning_popup():
-    with dpg.window(label="Warning", modal=True, tag="_warning", width=150,
-                                height=150):
-        dpg.add_text("Something went wrong!")
-        dpg.add_button(
-            label="OK",
-            callback=lambda:dpg.delete_item("_warning"))
 
-def ok_callback(_sender, app_data):
-    path = app_data['file_path_name']
+def warning_popup():
+    with dpg.window(label="Warning", modal=True, tag="_warning", width=250, height=150):
+        dpg.add_text("Something went wrong!")
+        dpg.add_button(label="OK", callback=lambda: dpg.delete_item("_warning"))
+
+
+def ok_callback(_sender, app_data, _user_data):
+    path = app_data["file_path_name"]
 
     # Generic check if a file exists
     if not os.path.isfile(path):
@@ -116,29 +145,37 @@ def ok_callback(_sender, app_data):
         return
 
     if path.endswith(".txt"):
-        with open(app_data['file_path_name'], 'r') as file:
+        with open(app_data["file_path_name"], "r") as file:
             for idx, line in enumerate(file):
-                dpg.set_value(f"_import_{idx}", line.strip("\r").strip('\n'))
+                dpg.set_value(f"_import_{idx}", line.strip("\r").strip("\n"))
             dpg.configure_item("_imported_text", show=True)
             dpg.configure_item("_import_0", color=(255, 255, 0, 255))
 
-    elif path.endswith('.jpeg') or path.endswith('.png'):
+    elif path.endswith(".jpeg") or path.endswith(".png"):
         width, height, _channels, data = dpg.load_image(path)
+
+        # Delete the previous texture + image if it exists
+        if dpg.does_item_exist("_image"):
+            dpg.delete_item("_image")
+            dpg.delete_item("_preview_image")
+
         with dpg.texture_registry(show=False):
             dpg.add_static_texture(
-                width=width, height=height, default_value=data, tag='_image')
+                width=width, height=height, default_value=data, tag="_image"
+            )
 
-        w_factor = 320/dpg.get_item_width('_image')
-        h_factor = 240/dpg.get_item_height('_image')
+        w_factor = 320 / dpg.get_item_width("_image")
+        h_factor = 240 / dpg.get_item_height("_image")
         scale_factor = min(w_factor, h_factor)
-        with dpg.window(label=app_data['file_name'], width=400, height=300,
-                        no_resize=True):
-            dpg.add_image(
-                '_image',
-                width=int(dpg.get_item_width('_image')*scale_factor),
-                height=int(dpg.get_item_width('_image')*scale_factor))
-            dpg.add_button(label="Convert", pos=(320, 260), tag='_convert',
-                           user_data=path, callback=button_callback)
 
+        dpg.add_image(
+            "_image",
+            tag="_preview_image",
+            parent="_preview_window",
+            width=int(dpg.get_item_width("_image") * scale_factor),
+            height=int(dpg.get_item_width("_image") * scale_factor),
+            before="_convert",
+        )
+        dpg.configure_item("_preview_window", show=True)
     else:
         warning_popup()
--- terminal/main.py
+++ terminal/main.py
@@ -1,9 +1,9 @@
 import sys
-import dearpygui.dearpygui as dpg
 
 import callbacks
+import dearpygui.dearpygui as dpg
+from display import add_display, init_display
 from serial_handler import SerialHandler
-from display import init_display, add_display
 
 if not sys.version_info >= (3, 6):
     raise EnvironmentError("Python version too old")
@@ -20,8 +20,8 @@ dpg.set_viewport_resize_callback(callbacks.viewport_resize_callback)
 with dpg.window(tag="_primary"):
     # Create Items
     dpg.add_input_text(
-        tag="_command", on_enter=True, hint="Command",
-        enabled=False, width=500)
+        tag="_command", on_enter=True, hint="Command", enabled=False, width=500
+    )
     dpg.add_button(tag="_send", label="Send", enabled=False, width=100)
     dpg.add_text("Disconnected", tag="_status", color=(255, 0, 0, 255))
 
@@ -38,54 +38,95 @@ with dpg.window(tag="_primary"):
     dpg.set_item_user_data("_command", ser)
 
     # Item positioning
-    dpg.set_item_pos("_send", (10, dpg.get_viewport_height()-75))
-    dpg.set_item_pos("_command", (70, dpg.get_viewport_height()-75))
+    dpg.set_item_pos("_send", (10, dpg.get_viewport_height() - 75))
+    dpg.set_item_pos("_command", (70, dpg.get_viewport_height() - 75))
     dpg.set_item_pos(
-        "_status", 
-        (dpg.get_viewport_width()-len(dpg.get_value("_status"))-100, 20))
+        "_status", (dpg.get_viewport_width() - len(dpg.get_value("_status")) - 100, 20)
+    )
 
 # File dialog
-with dpg.file_dialog(directory_selector=False, show=False,
-                    callback=callbacks.ok_callback, tag="_file_dialog",
-                    width=700 ,height=400):
+with dpg.file_dialog(
+    directory_selector=False,
+    show=False,
+    callback=callbacks.ok_callback,
+    tag="_file_dialog",
+    width=700,
+    height=400,
+):
     dpg.add_file_extension("Scripts (*.txt){.txt}")
     dpg.add_file_extension("Images (*.png *.jpeg){.png,.jpeg}")
 
+# Image preview
+with dpg.window(
+    label="placeholder",
+    width=400,
+    height=400,
+    no_resize=True,
+    tag="_preview_window",
+    show=False,
+):
+    dpg.add_button(
+        label="Convert",
+        pos=(320, 260),
+        tag="_convert",
+        callback=callbacks.button_callback,
+        width=70,
+    )
+    dpg.add_combo(
+        tag="_target_size_image",
+        label="Target size",
+        items=("8", "16", "32", "64"),
+        width=dpg.get_item_configuration("_convert")["width"],
+        default_value="16",
+    )
+
 # Window for imported text.
-with dpg.window(tag="_imported_text", show=False, autosize=False,
-                width=700, height=400, no_scrollbar=True):
+with dpg.window(
+    tag="_imported_text",
+    show=False,
+    autosize=False,
+    width=700,
+    height=400,
+    no_scrollbar=True,
+):
     # Add lines for imported text
     for idx in range(100):
-        dpg.add_text("", tag=f"_import_{idx}", pos=(20, 20+idx*13))
+        dpg.add_text("", tag=f"_import_{idx}", pos=(20, 20 + idx * 13))
 
     # Add buttons
     dpg.add_button(
         label="Step over",
         tag="_step_over",
         pos=(
-            dpg.get_item_width("_imported_text")-100,
-            dpg.get_item_height("_imported_text")-90),
+            dpg.get_item_width("_imported_text") - 100,
+            dpg.get_item_height("_imported_text") - 90,
+        ),
         width=80,
         callback=callbacks.button_callback,
-        user_data=ser)
+        user_data=ser,
+    )
     dpg.add_button(
         label="Skip",
         tag="_skip",
         pos=(
-            dpg.get_item_width("_imported_text")-100,
-            dpg.get_item_height("_imported_text")-60),
+            dpg.get_item_width("_imported_text") - 100,
+            dpg.get_item_height("_imported_text") - 60,
+        ),
         width=80,
         callback=callbacks.button_callback,
-        user_data=ser)
+        user_data=ser,
+    )
     dpg.add_button(
         label="Send all",
         tag="_send_all",
         pos=(
-            dpg.get_item_width("_imported_text")-100,
-            dpg.get_item_height("_imported_text")-30),
+            dpg.get_item_width("_imported_text") - 100,
+            dpg.get_item_height("_imported_text") - 30,
+        ),
         width=80,
         callback=callbacks.button_callback,
-        user_data=ser)
+        user_data=ser,
+    )
 
 # Handler for window resizing (instead of viewport)
 with dpg.item_handler_registry(tag="_window_handler"):
@@ -95,43 +136,45 @@ dpg.bind_item_handler_registry("_imported_text", "_window_handler")
 # Menu bar
 with dpg.viewport_menu_bar():
     with dpg.menu(label="File"):
-        dpg.add_menu_item(label="Open",
-                          callback=lambda:dpg.show_item("_file_dialog"))
+        dpg.add_menu_item(label="Open", callback=lambda: dpg.show_item("_file_dialog"))
     with dpg.menu(label="Serial"):
         with dpg.menu(label="Port"):
             for port in ser.available_ports:
                 dpg.add_menu_item(
-                    tag = f"{port.device}",
+                    tag=f"{port.device}",
                     label=f"{port.device} {port.description}",
-                    callback=callbacks.menu_callback)
+                    callback=callbacks.menu_callback,
+                )
                 dpg.set_item_user_data(f"{port.device}", ser)
         with dpg.menu(label="Interval"):
             dpg.add_slider_int(
-                label="ms", 
-                tag="_interval_slider", 
-                min_value=0, 
-                max_value=100, 
-                callback=
-                lambda:dpg.set_value("_interval_slider", 0) 
-                    if dpg.get_value("_interval_slider") < 0
-                    else dpg.set_value(
-                        "_interval_slider", 
-                        dpg.get_value("_interval_slider")))
+                label="ms",
+                tag="_interval_slider",
+                min_value=0,
+                max_value=100,
+                callback=lambda: dpg.set_value("_interval_slider", 0)
+                if dpg.get_value("_interval_slider") < 0
+                else dpg.set_value(
+                    "_interval_slider", dpg.get_value("_interval_slider")
+                ),
+            )
     with dpg.menu(label="Tools"):
         dpg.add_menu_item(
-            label="Show Metrics",
-            callback=lambda:dpg.show_tool(dpg.mvTool_Metrics))
+            label="Show Metrics", callback=lambda: dpg.show_tool(dpg.mvTool_Metrics)
+        )
         dpg.add_menu_item(
-            label="Show Debug", callback=lambda:dpg.show_tool(dpg.mvTool_Debug))
+            label="Show Debug", callback=lambda: dpg.show_tool(dpg.mvTool_Debug)
+        )
         dpg.add_menu_item(
-            label="Show Style Editor",
-            callback=lambda:dpg.show_tool(dpg.mvTool_Style))
+            label="Show Style Editor", callback=lambda: dpg.show_tool(dpg.mvTool_Style)
+        )
         dpg.add_menu_item(
-            label="Show Font Manager",
-            callback=lambda:dpg.show_tool(dpg.mvTool_Font))
+            label="Show Font Manager", callback=lambda: dpg.show_tool(dpg.mvTool_Font)
+        )
         dpg.add_menu_item(
             label="Show Item Registry",
-            callback=lambda:dpg.show_tool(dpg.mvTool_ItemRegistry))
+            callback=lambda: dpg.show_tool(dpg.mvTool_ItemRegistry),
+        )
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
