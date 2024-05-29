1c985ea63db669a57667430c8b30058bf8c277f8
Mirkoet <MirkovdHoef@gmail.com>
2024-05-29 08:00:52 +0200

    #37 removed deprecated function and small changes

--- app/Core/Src/CHAL.cpp
+++ app/Core/Src/CHAL.cpp
@@ -1,8 +1,7 @@
 
 /**
- * @file uart_init.c
- * @brief UART initialization functions.
- * @author @Mirkoet
+ * @file CHAL.cpp
+ * @brief UART and DMA initialization functions.
  */
 
 #include "CHAL.h"
@@ -16,10 +15,6 @@
 
 using namespace std;
 std::queue<std::string> incoming_commands_q;
-
-// both temp until cpp is fixed
-uint8_t tempMainBuffer[2048];
-int offset = 0;
 
 /**
  * @brief Initializes the UART and GPIO interface by calling the GPIO and UART init functions.
@@ -331,27 +326,6 @@ void CHAL_clear_idledetect()
     (void)tmpreg;
     tmpreg = USART2->DR; // data register;
     (void)tmpreg;
-}
-
-/**
- * @brief An older polled function to move the data from the DMA receive buffer to a larger array.
- *
- * This should be replaced with CPP queue, but for now: The data is send to a larger array to form a queue that can be used by the following layers.
- * The DMA receive buffer can also just be larger, but it needs to be put into a queue in the future
- *
- * The function can handle one line at a time (until LF)
- *
- *
- * @return void This function does not return a value.
- */
-void CHAL_event_call_back(uint8_t* rx_buff, uint16_t bufferlength)
-{
-    strcpy((char*)(tempMainBuffer) + offset, (char*)rx_buff); // copy uart/dma receive buffer into new (larger) buffer. Offset is used to prevent wrinting over previous data
-    CHAL_disable_DMA(DMA1_Stream5); // to change the NDTR register the DMA NEEDS to be disabled first.
-    offset += (bufferlength - DMA1_Stream5->NDTR) + 1;
-    DMA1_Stream5->NDTR = bufferlength; // reset RX-buff pointer to start
-    memset(rx_buff, 0, bufferlength); // reset rx_buff for new reception
-    CHAL_enable_DMA(DMA1_Stream5); // restart the DMA for UART reception
 }
 
 /**
