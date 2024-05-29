99908361af233636488c0177845c28775c518846
Jesse <swildensjesse@gmail.com>
2024-05-29 20:16:23 +0200

    #46 Cppcheck generates its own error codes if an error has been found

--- app/cppcheck.sh
+++ app/cppcheck.sh
@@ -5,14 +5,4 @@ touch $(dirname "$0")/cppcheck-result.xml
 
 # $n == argument n (0 == file, 1 == argument 1, etc.., $@ == all arguments)
 # (>) redirects, 1 == stdout, 2 == stderr
-cppcheck --cppcheck-build-dir=$(dirname "$0")/build-cppcheck --enable=all --check-level=exhaustive --xml $@ 2> $(dirname "$0")/cppcheck-result.xml
-
-result=$(<$(dirname "$0")/cppcheck-result.xml)
-
-if [[ $result == *"<error "* ]] || [[ $result == *"<error>"* ]]; then
-    # Error is detected in xml file
-    echo $result
-    exit 1 # Try again ;)
-else
-    exit 0
-fi
+cppcheck --cppcheck-build-dir=$(dirname "$0")/build-cppcheck --enable=all --check-level=exhaustive --error-exitcode=2 --xml $@ 2> $(dirname "$0")/cppcheck-result.xml
