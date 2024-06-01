888cd871cb302a417e046407c7c0a40a264783a2
JesseSwildens <swildensjesse@gmail.com>
2024-06-01 18:00:22 +0200

    #8 Added unit tests with 2 example tests

---
+++ app/API/mock/stm32f4xx.h
@@ -0,0 +1,6 @@
+#ifndef STM32F4XX_H
+#define STM32F4XX_H
+
+void SystemInit(void);
+
+#endif
---
+++ app/API/mock/vga_mock.c
@@ -0,0 +1,14 @@
+#include "stm32_ub_vga_screen.h"
+#include <stdint.h>
+
+uint8_t VGA_RAM1[(VGA_DISPLAY_X + 1) * VGA_DISPLAY_Y];
+
+void SystemInit(void)
+{
+    return;
+}
+
+void UB_VGA_Screen_Init(void)
+{
+    return;
+}
