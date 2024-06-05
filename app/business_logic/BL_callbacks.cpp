8b9630d8d211f517f2763b6e3c0b5075421d09ab
Mirkoet <MirkovdHoef@gmail.com>
2024-06-05 19:12:27 +0200

    live review with jesse, deleted deprecated functions ande defines

--- app/business_logic/BL_callbacks.cpp
+++ app/business_logic/BL_callbacks.cpp
@@ -27,34 +27,7 @@ extern "C"
 #define SYSTICK_LOAD (SystemCoreClock / 1000000U)
 #define SYSTICK_DELAY_CALIB (SYSTICK_LOAD >> 1)
 
-/**
- * @brief Delay in us function using systick
- *
- */
-#define DELAY_US(us)                                                \
-    do                                                              \
-    {                                                               \
-        uint32_t start = SysTick->VAL;                              \
-        uint32_t ticks = (us * SYSTICK_LOAD) - SYSTICK_DELAY_CALIB; \
-        while ((start - SysTick->VAL) < ticks)                      \
-            ;                                                       \
-    } while (0)
-
-/**
- * @brief Delay in ms function using systick
- *
- */
-#define DELAY_MS(ms)                                \
-    do                                              \
-    {                                               \
-        for (uint32_t i = 0; i < (uint32_t)ms; ++i) \
-        {                                           \
-            DELAY_US(1000);                         \
-        }                                           \
-    } while (0)
-
 // #define BL_DEBUG_COMMANDS
-#define ECHO_REPEATS
 
 using namespace std;
 
